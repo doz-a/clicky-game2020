fcfe5aa7a725c6789739ee406937971a15dbc8bf
Miguel Delos Santos <mendoza.d.santos@gmail.com>
2020-02-29 13:49:24 -0800

    Updates

---
+++ precache-manifest.3f6b8e7acbe5ed00b33cb91d748304ab.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac0e2504a8d15766736499ab635a0acf",
+    "url": "/clicky-game2020/index.html"
+  },
+  {
+    "revision": "18598a124f47ba76e56e",
+    "url": "/clicky-game2020/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "ab088de030227c22a9d1",
+    "url": "/clicky-game2020/static/css/main.a8e7e614.chunk.css"
+  },
+  {
+    "revision": "18598a124f47ba76e56e",
+    "url": "/clicky-game2020/static/js/2.bdc9b4fa.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/clicky-game2020/static/js/2.bdc9b4fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab088de030227c22a9d1",
+    "url": "/clicky-game2020/static/js/main.93759be5.chunk.js"
+  },
+  {
+    "revision": "0fe368379acad15d3998",
+    "url": "/clicky-game2020/static/js/runtime-main.3683e528.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clicky-game2020/static/media/logo.5d5d9eef.svg"
+  }
+]);
